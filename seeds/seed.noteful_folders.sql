881212115d1ac6259239d789ff7c23ceb9609f7f
Stacey Sugiono <sugiono.stacey@gmail.com>
2020-01-29 13:38:26 -0800

    added migrations and seeds

---
+++ seeds/seed.noteful_folders.sql
@@ -0,0 +1,6 @@
+INSERT INTO noteful_folders (folder_name)
+VALUES
+    ('Folder One'),
+    ('Folder Two'),
+    ('Folder Three'),
+    ('Folder Four');
---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,6 @@
+INSERT INTO noteful_notes (note_name, content, folder_id)
+VALUES
+    ('Apple', 'apple content', 1),
+    ('Banana', 'banana content', 2),
+    ('Cats', 'meow', 3),
+    ('Dogs', 'woof', 4);
